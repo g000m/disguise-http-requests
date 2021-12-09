bcfecc3a7f1f35477609207772d85d4d5b19179f
Gabe Herbert <gh@gabeherbert.com>
2021-12-09 10:19:54 -0800

    fix class names

--- src/functions.php
+++ src/functions.php
@@ -6,7 +6,7 @@ add_action( 'init', function () {
 	add_filter( 'http_request_args', 'Disguise\disguise_request_args', 10, 2 );
 
 	// @TODO pass these in via settings
-	if ( class_exists( '\BuddyBossApp\Build' ) && class_exists( '\BuddyBossApp\Admin\Build\Build' ) ) {
+	if ( class_exists( 'BuddyBossApp\Build' ) && class_exists( 'BuddyBossApp\Admin\Build\Build' ) ) {
 		remove_filter( 'admin_notices', [ \BuddyBossApp\Build::instance(), 'app_core_version_admin_notice' ] );
 		remove_filter( 'admin_notices', [ \BuddyBossApp\Admin\Build\Build::instance(), 'admin_notices' ] );
 	}
